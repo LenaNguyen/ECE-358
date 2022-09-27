a65b256d8fa96832c0e16059ac5cc0b070e800d7
Annie Zou <x2zou@uwaterloo.ca>
2022-09-27 11:43:28 -0400

    fixing and/or logic bug

--- Lab1/RandomGenerator.py
+++ Lab1/RandomGenerator.py
@@ -7,18 +7,21 @@ import statistics
 lambd = 75
 number = 1000
 
+def exp_rand(lamb):
+    random.seed()
+    u = random.random()
+    distr = -(1/lambd) * math.log(1-u)
+    return distr
 
-def exp_rand(lamb, num):
-    ret = []
-    for _ in range(0, num):
-        random.seed()
-        u = random.random()
-        distr = -(1/lambd) * math.log(1-u)
-        ret.append(distr)
-    return ret
+def exp_rand_time_span(lamb, time):
+    arrival_time_arr = []
+    run_sum = 0
+    while(len(arrival_time_arr) < 100000 and run_sum < 1000):
+        run_sum += exp_rand(lamb)
+        arrival_time_arr.append(run_sum)
+    return arrival_time_arr
 
-
-dist = exp_rand(lambd, number)
+dist = exp_rand_time_span(lambd, number)
 mean = statistics.mean(dist)
 varianc = statistics.variance(dist)
 
