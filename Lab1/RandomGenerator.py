b45153718fe5ddd2e7a0f27ee826f30d3f466803
LenaNguyen27 <nguyen.lena27@hotmail.com>
2022-09-27 14:11:12 -0400

    pseudocode and format Q1

--- Lab1/RandomGenerator.py
+++ Lab1/RandomGenerator.py
@@ -1,31 +1,7 @@
 import math
-import random
 import numpy as np
-import matplotlib.pyplot as plt
-import statistics
 
-lambd = 75
-number = 1000
 
-def exp_rand(lamb):
-    random.seed()
-    u = random.random()
-    distr = -(1/lambd) * math.log(1-u)
-    return distr
-
-def exp_rand_time_span(lamb, time):
-    arrival_time_arr = []
-    run_sum = 0
-    while(len(arrival_time_arr) < 100000 and run_sum < 1000):
-        run_sum += exp_rand(lamb)
-        arrival_time_arr.append(run_sum)
-    return arrival_time_arr
-
-dist = exp_rand_time_span(lambd, number)
-mean = statistics.mean(dist)
-varianc = statistics.variance(dist)
-
-count, bins, ignored = plt.hist(dist, 14, density=True)
-plt.show()
-print(mean)
-print(varianc)
+def gen_exponential_val(_lambda):
+    u = np.random.uniform(0, 1)
+    return -(1/_lambda) * math.log(1-u)
--- Lab1/main.py
+++ Lab1/main.py
@@ -1,8 +1,72 @@
 import numpy as np
 import RandomGenerator
+import statistics
+
+
+def question1():
+    print("**QUESTION 1**")
+    Q1_LAMBDA = 75
+    Q1_COUNT = 1000
+    result = []
+    for _ in range(Q1_COUNT):
+        result.append(RandomGenerator.gen_exponential_val(Q1_LAMBDA))
+
+    expected_mean = 1/75
+    expected_variance = 1/(Q1_LAMBDA**2)
+    mean = statistics.mean(result)
+    _variance = statistics.variance(result)
+    print(f'Expected mean: {expected_mean}, Actual mean: {mean}')
+    print(
+        f'Expected variance: {expected_variance}, Actual variance: {_variance}')
+    print("\n")
 
 
 def m_m_1_queue():
-    _lamdba = 75
-    _arrive_time_number = 1000
-    arrival_times = np.random.exponential(1/_lamdba, 1000)
+    # rho values are 0.25 to 0.95
+    rho_values = [0.35, 0.45, 0.55, 0.65, 0.75, 0.85]
+    T = 1000
+    L = 2000
+    C = 1000000
+
+    """
+	Generate Packet Arrival
+		lambda = rho * C / L
+		T = 1000
+
+	Generate Packet Length
+		lambda = 1/2000
+		num_lengths = num_arrivals
+	
+	Generate service time
+		for each pkt(i), L(i) / C
+
+    """
+
+    """
+        Generate packet departure
+                if queue has packets. arrival pkt(i) >= departure(i - 1):
+                        departure time pkt(i) = departure pkt(i-1) + service time pkt(i)
+                else
+                        departure time pkt(i) = arrival pkt(i) + service time pkt(i) 
+
+        """
+
+    """
+	Generate Observer Events
+		generate random observation events according to packet arrival distirbution
+		rate of 5 times more than packet arrival
+			- lammbda * 5
+		Observer Event - record state of queue:
+			- E[N]: avg of num packets in queue (Na - Nd) / No
+			- P_idle: proportion of time the server is idle (system is empty): is_idle = Na == Nd ? 1 : 0 
+			- P_Loss: probability the packet will be dropped, due to buffer being full
+	"""
+    """
+	Creating the DES?
+		Put all events in a list and sort them by time
+		Based on event type, increment Na, Nd, and No
+			if event is observer, calculate the performance metrics
+	"""
+
+
+question1()
